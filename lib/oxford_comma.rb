dc5a9637a9b8f678f8e71164d7a21d24b2c4494f
Maria Yimei Shao <maria.yimei.shao@gmail.com>
2019-08-26 00:24:50 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+if array.size == 1 
+array[0]
+elsif array.size == 2 
+array.join(" and ")
+else array.size >= 3
+array[-1] = "and #{array[-1]}"
+array.join(",")
+end
 end
